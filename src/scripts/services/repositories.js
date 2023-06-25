9309162db21fde7facf91d68b8d6eac9505a9eb8
Walassi Silva <walassi.b@gmail.com>
2023-06-25 17:12:58 -0300

    corrindo caminhos de import

--- src/scripts/services/repositories.js
+++ src/scripts/services/repositories.js
@@ -1,4 +1,4 @@
-import { baseUrl, repositoriesQtde} from '/src/scripts/variables.js'
+import { baseUrl, repositoriesQtde} from '../variables.js'
 
 async function getRepositories(userName){
     const response = await fetch(`${baseUrl}/${userName}/repos?per_page=${repositoriesQtde}`)
